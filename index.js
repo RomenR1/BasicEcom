5ec8dff25eac475a55ff3aff69bf9e5586cf6444
RomenR1 <romenakholiya34@gmail.com>
2023-08-24 13:24:50 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = express();
 const Router = require("./ROUTER/router.js");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 main().catch((err) => console.log(err));
 
@@ -14,11 +15,13 @@ async function main() {
 // Apply middlewares
 server.use(express.json()); // Use express.json() once
 server.use(cors({ origin: "*" })); // Allow requests from any origin
+server.use(express.static(path.join(__dirname, "dist")));
 
 // Define routes
 server.use("/products", Router.PoroductRouter); // Fixed typo here
 
 // Start the server
-server.listen(process.env.POST, () => {
+// console.log(process.env.POST)
+server.listen(process.env.PORT, () => {
   console.log("Server started on port 8080");
 });
